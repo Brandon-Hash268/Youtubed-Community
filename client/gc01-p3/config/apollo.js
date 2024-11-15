be83b29f9b060fe0b72235874f36c4ac49edc43d
Brandon Vincentius <brandonvincentius54321@gmail.com>
2024-11-15 21:29:58 +0700

    refactor:change ngrok to deployed server

--- client/gc01-p3/config/apollo.js
+++ client/gc01-p3/config/apollo.js
@@ -3,7 +3,7 @@ import { setContext } from "@apollo/client/link/context";
 import * as SecureStore from "expo-secure-store";
 
 const httpLink = createHttpLink({
-  uri: "https://9522-2001-448a-20a0-b6b-68ec-4274-6ad-713c.ngrok-free.app/",
+  uri: "https://ip-p2.brandon-hash268.online/",
 });
 
 const authLink = setContext((_, { headers }) => {
